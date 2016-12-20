a9b34acf62a3143f99db4a9f86f885c1e993416b
Mikhail Shilkov <shilkov@gmail.com>
2016-12-20 22:47:14 +0100

    Day 5 - comment and small refactoring

--- Day5.fs
+++ Day5.fs
@@ -43,12 +43,35 @@ let getPassword1 door =
   |> Seq.take 8
   |> toString
 
+/// Instead of simply filling in the password from left to right, the hash 
+/// now also indicates the position within the password to fill. You still look 
+/// for hashes that begin with five zeroes; however, now, the sixth character 
+/// represents the position (0-7), and the seventh character is the character 
+/// to put in that position.
+/// 
+/// A hash result of 000001f means that f is the second character in the password. 
+/// Use only the first result for each position, and ignore invalid positions.
+/// 
+/// For example, if the Door ID is abc:
+/// 
+/// The first interesting hash is from abc3231929, which produces 0000015...; 
+/// so, 5 goes in position 1: _5______.
+/// In the previous method, 5017308 produced an interesting hash; however, 
+/// it is ignored, because it specifies an invalid position (8).
+/// The second interesting hash is at index 5357525, which produces 000004e...; 
+/// so, e goes in position 4: _5__e___.
+/// You almost choke on your popcorn as the final character falls into place, 
+/// producing the password 05ace8e3.
+/// 
+/// Given the actual Door ID and this new method, what is the password?
+
 let getPassword2 door =
   Seq.initInfinite id
   |> Seq.map (fun i -> door + i.ToString() |> md5)
   |> Seq.filter (fun hash -> hash.StartsWith "00000" && hash.[5] >= '0' && hash.[5] <= '7')
-  |> Seq.distinctBy (fun hash -> hash.[5])
+  |> Seq.map (fun hash -> hash.[5], hash.[6])
+  |> Seq.distinctBy fst
   |> Seq.take 8
-  |> Seq.sortBy (fun hash -> hash.[5])
-  |> Seq.map (fun hash -> hash.[6])
+  |> Seq.sortBy fst
+  |> Seq.map snd
   |> toString
